4eb8de54e280132adac7ac2f2a7c536c94670399
Emmanuel <emmanuelmercadogranados@gmail.com>
2024-10-27 22:31:38 -0600

    Cambios config

--- resources/js/Components/Table.jsx
+++ resources/js/Components/Table.jsx
@@ -1,8 +1,8 @@
 export default function Table({ items, columns, primary, action }) {
     return (
         <div className="relative overflow-x-auto border shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <table className="w-full text-sm text-left text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
                 <tr>
                     <th scope="col" className="px-6 py-3">{primary}</th>
                     {columns.map((column) =>
@@ -13,8 +13,8 @@ export default function Table({ items, columns, primary, action }) {
                 </thead>
                 <tbody>
                 {items.map((item) =>
-                    <tr key={item.id} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <tr key={item.id} className="bg-white border-b">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                             #{item.id}
                         </th>
                         {columns.map((column) =>
@@ -23,7 +23,7 @@ export default function Table({ items, columns, primary, action }) {
                             </td>
                         )}
                         <td className="px-6 py-4">
-                            <a href={route(action, item.id)} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">View Details</a>
+                            <a href={route(action, item.id)} className="font-medium text-blue-600 hover:underline">View Details</a>
                         </td>
                     </tr>
                 )}
